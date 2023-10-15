cd91e6336535e27008d609984ca59e523c4e6972
Nate Butler <iamnbutler@gmail.com>
2023-10-15 10:40:27 -0400

    Refactor game page and remove unused game start and story files

--- engine/app/page.tsx
+++ engine/app/page.tsx
@@ -1,28 +1,24 @@
-"use client";
+'use client';
+import { Step } from "./ui/step";
+import { useMessagesStore } from "./stores/messages";
+import { Suspense } from "react";
 
-import Link from "next/link";
-import { useState } from "react";
-import Typewriter from "react-ts-typewriter";
-
-export default function Home() {
-  const [showContinue, setShowContinue] = useState(false);
-
-  return (
-    <>
-      <h1>The Fall of Atheria</h1>
-      <Typewriter
-        onFinished={() => setShowContinue(true)}
-        speed={10}
-        text="In the land of Atheria, once a powerful and prosperous kingdom, ruin and despair now reign. Magic, the kingdom's former backbone, has dwindled into a scarce and dangerous resource. The arcane arts, previously revered, are now feared and strictly regulated by a royal guild known as the Silver Hand. Established by the king to investigate the cause of Atheria's downfall, the guild is determined to restore the kingdom to its former glory."
-      />
-      <Link
-        href="/start"
-        className={`block underline mt-2 hover:opacity/60 ${
-          showContinue ? "" : "hidden"
-        }`}
-      >
-        Continue
-      </Link>
-    </>
-  );
+function Home() {
+    const messages = useMessagesStore((state) => state.messages);
+    return (
+        <div className="flex flex-col gap-4">
+            {messages.map((message, ix) => (
+                <Suspense fallback={<div>Loading...</div>} key={`${ix}-step`}>
+                    <Step index={ix} stepContent={message} />
+                </Suspense>))}
+        </div>
+    );
 }
+
+export default Home;
+
+// <Typewriter
+//   onFinished={() => setShowContinue(true)}
+//   speed={10}
+//   text="In the land of Atheria, once a powerful and prosperous kingdom, ruin and despair now reign. Magic, the kingdom's former backbone, has dwindled into a scarce and dangerous resource. The arcane arts, previously revered, are now feared and strictly regulated by a royal guild known as the Silver Hand. Established by the king to investigate the cause of Atheria's downfall, the guild is determined to restore the kingdom to its former glory."
+// />
