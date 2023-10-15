935162af1a942c9415e24156f7ef48906934dbae
Nate Butler <iamnbutler@gmail.com>
2023-10-15 10:55:45 -0400

    Update layout with grid system and add navigation section

--- engine/app/layout.tsx
+++ engine/app/layout.tsx
@@ -16,9 +16,15 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <main className="flex min-h-screen gap-16 justify-center p-24 font-mono text-sm">
+                <main className="h-screen w-screen p-8 font-mono text-sm max-w-7xl grid grid-cols-12 mx-auto gap-8 overflow-hidden">
                     {ENABLE_DEBUG && <Debug />}
-                    <section className="w-[540px]">
+                    <nav className="border border-white/10 overflow-y-scroll p-8 col-span-6">
+                        {Array.from({ length: 100 }, (_, i) => (
+                            <li key={i}>{`Item ${i + 1}`}</li>
+                        ))}
+                    </nav>
+                    <section className="max-w-[600px] col-span-6 border border-white/10 p-8 overflow-y-scroll"
+                    >
                         <div
                             className={`flex flex-col prose prose-sm dark:prose-invert max-w-none prose-pre:leading-none prose-code:leading-none
                         `}
