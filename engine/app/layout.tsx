5656016f323a77ad8feab2cd5fc093fa5b88bc82
Nate Butler <iamnbutler@gmail.com>
2023-10-15 11:26:09 -0400

    Update CSS and HTML to improve scrolling behavior and adjust color scheme

--- engine/app/layout.tsx
+++ engine/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className="bg-black text-white">
             <body>
                 {children}
             </body>
