9d66c8da6b1cfacec39ec49fb1246156789aeb01
Nate Butler <iamnbutler@gmail.com>
2023-10-15 20:44:16 -0400

    Add Seraphina Grey character sheet to characters library

---
+++ engine/app/lib/characters/seraphina.ts
@@ -0,0 +1,15 @@
+import { CharacterSheet } from "..";
+
+export const seraphina_character_sheet: CharacterSheet = {
+    name: 'Seraphina Grey',
+    given_names: ['Seraphina'],
+    family_name: 'Grey',
+    lineage: 'Human',
+    level: 1,
+    classes: [
+        {
+            name: 'Paladin',
+            level: 1,
+        }
+    ],
+}
