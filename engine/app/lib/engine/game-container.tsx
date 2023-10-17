b0632795185e0ef146e7d2670c0b10c7fc4a359e
Nate Butler <iamnbutler@gmail.com>
2023-10-17 11:56:31 -0400

    Finish basic scenario generation

--- engine/app/lib/engine/game-container.tsx
+++ engine/app/lib/engine/game-container.tsx
@@ -1,79 +1,68 @@
 "use client";
-import { LoadingIndicator, TextOutput, useGame } from "./core";
+import { LoadingIndicator, TextOutput, generateScenario, useGame } from "./core";
 import { VStack } from "../stack";
 import { ContextHeader } from "@/app/ui/context-header";
 import { Choice } from "@/app/ui/choices";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-interface GameContainerProps {}
+interface GameContainerProps { }
 
 export const GameContainer: React.FC<GameContainerProps> = () => {
-  const [gameState, dispatch] = useGame();
-  const [nextScenarioLoading, setNextScenarioLoading] = useState(false);
+    const [gameState, dispatch] = useGame();
+    const [nextScenarioLoading, setNextScenarioLoading] = useState(false);
 
-  useEffect(() => {
-    if (gameState.scenario.content.isLoaded) {
-      setNextScenarioLoading(false);
-    }
-  }, [gameState.scenario.content.isLoaded]);
+    const handleChoose = async (choiceIndex: number) => {
+        dispatch({ type: 'SELECT_OPTION', payload: choiceIndex });
+        setNextScenarioLoading(true);
+        const newScenario = await generateScenario(gameState.scenario.id + 1, gameState);
+        if (newScenario) {
+            dispatch({ type: 'NEXT_SCENARIO', payload: newScenario });
+            setNextScenarioLoading(false);
+        }
+    };
 
-  const moveToNextScenario = () => {
-    dispatch({ type: "NEXT_SCENARIO" });
-    setNextScenarioLoading(true);
-  };
+    return (
+        <>
+            <div className="fixed bottom-4 left-4 opacity-100 text-[9px] text-white max-w-xs leading-none">
+                <ul className="flex flex-col gap-3">
+                    <li>{nextScenarioLoading.toString()}</li>
+                    <li>{JSON.stringify(gameState.scenario)}</li>
+                </ul>
+            </div>
+            <VStack size={"lg"}>
+                {/* Display past scenarios with faded text */}
+                {gameState.pastScenarios.map((scenario, index) => (
+                    <VStack
+                        size={"md"}
+                        key={index}
+                        className="text-white/50"
+                        id={`scenario-${index}`}
+                    >
+                        <ContextHeader title="Scenario" subtitle={scenario.id.toString()} />
+                        <TextOutput content={scenario.content.message.content} />
+                        <Choice scenario={scenario} onChoose={handleChoose} />
+                    </VStack>
+                ))}
+                {/* Show current scenario */}
 
-  const handleChoose = (choice: string, choiceIndex: number) => {
-    dispatch({ type: "SELECT_OPTION", payload: choiceIndex });
-    moveToNextScenario();
-  };
-
-  return (
-    <>
-      <div className="fixed bottom-4 left-4 opacity-100 text-[9px] text-white max-w-xs leading-none">
-        <ul className="flex flex-col gap-3">
-          <li>{nextScenarioLoading.toString()}</li>
-          <li>{JSON.stringify(gameState.scenario)}</li>
-        </ul>
-      </div>
-      <VStack size={"lg"}>
-        {/* Display past scenarios with faded text */}
-        {gameState.pastScenarios.map((scenario, index) => (
-          <VStack
-            size={"md"}
-            key={index}
-            className="text-white/50"
-            id={`scenario-${index}`}
-          >
-            <ContextHeader title="Scenario" subtitle={scenario.id.toString()} />
-            <TextOutput content={scenario.content.message.content} />
-            <Choice scenario={scenario} onChoose={handleChoose} />
-          </VStack>
-        ))}
-        {/* Show current scenario */}
-
-        <VStack size={"md"} className="text-white" id={"current-scenario"}>
-          {nextScenarioLoading ? (
-            <LoadingIndicator />
-          ) : (
-            <>
-              <ContextHeader
-                title="Scenario"
-                subtitle={gameState.scenario.id.toString()}
-              />
-              <TextOutput
-                content={gameState.scenario.content.message.content}
-              />
-              {gameState.scenario.content.choices.length > 0 && (
-                <Choice
-                  scenario={gameState.scenario}
-                  onChoose={handleChoose}
-                  current
-                />
-              )}
-            </>
-          )}
-        </VStack>
-      </VStack>
-    </>
-  );
+                <VStack size={"md"} className="text-white" id={"current-scenario"}>
+                    <ContextHeader
+                        title="Scenario"
+                        subtitle={gameState.scenario.id.toString()}
+                    />
+                    <TextOutput
+                        content={gameState.scenario.content.message.content}
+                    />
+                    {gameState.scenario.content.choices.length > 0 && (
+                        <Choice
+                            scenario={gameState.scenario}
+                            onChoose={handleChoose}
+                            current
+                        />
+                    )}
+                    {nextScenarioLoading && <LoadingIndicator />}
+                </VStack>
+            </VStack>
+        </>
+    );
 };
--- engine/app/stores/messages.ts
+++ engine/app/stores/messages.ts
@@ -1,77 +1,75 @@
 import { create } from "zustand";
 
 export const INITIAL_MESSAGE: UIMessage = {
-  isLoaded: true,
-  message: {
-    content: [
-      "You are Seraphina Grey, a fledgling Paladin of the Church of Tyr, the God of Justice. Raised among the nobility, you've chosen a different path, dedicating yourself to the cause of righteousness.",
-      "Over the years, you've dragged your body through gruelling physical training, and persevered to attune your spirit to the divine energies that you command. Though the arcane arts don't come naturally to you, your ironclad determination pushes you through all manner of challenges.",
-      `Today finds you in the practice yard, finishing your martial training for the day. As you wipe the sweat from your brow, you look around the bustling city of Waterdeep busy with commerce and noise. It's an exciting place, full of potential adventure and danger.`,
-      "It's time to carve your own path in this vast and unpredictable world.",
+    message: {
+        content: [
+            "You are Seraphina Grey, a fledgling Paladin of the Church of Tyr, the God of Justice. Raised among the nobility, you've chosen a different path, dedicating yourself to the cause of righteousness.",
+            "Over the years, you've dragged your body through gruelling physical training, and persevered to attune your spirit to the divine energies that you command. Though the arcane arts don't come naturally to you, your ironclad determination pushes you through all manner of challenges.",
+            `Today finds you in the practice yard, finishing your martial training for the day. As you wipe the sweat from your brow, you look around the bustling city of Waterdeep busy with commerce and noise. It's an exciting place, full of potential adventure and danger.`,
+            "It's time to carve your own path in this vast and unpredictable world.",
+        ],
+        step: 1,
+    },
+    choices: [
+        {
+            id: 1,
+            text: "Head to the local tavern to gather information on any pressing issues around Waterdeep.",
+        },
+        {
+            id: 2,
+            text: "Visit the town board to look for any quests or jobs suitable for a Paladin.",
+        },
+        {
+            id: 3,
+            text: "Take a stroll around the city, helping those in need and spreading goodwill as befits a Paladin.",
+        },
+        {
+            id: 4,
+            text: "Go to the Church to pray for guidance and seek any divine quests that might be assigned to you.",
+        },
+        {
+            id: 5,
+            text: "Visit the bustling marketplace, interacting with the locals or possibly purchasing necessary adventuring gear.",
+        },
     ],
-    step: 1,
-  },
-  choices: [
-    {
-      id: 1,
-      text: "Head to the local tavern to gather information on any pressing issues around Waterdeep.",
-    },
-    {
-      id: 2,
-      text: "Visit the town board to look for any quests or jobs suitable for a Paladin.",
-    },
-    {
-      id: 3,
-      text: "Take a stroll around the city, helping those in need and spreading goodwill as befits a Paladin.",
-    },
-    {
-      id: 4,
-      text: "Go to the Church to pray for guidance and seek any divine quests that might be assigned to you.",
-    },
-    {
-      id: 5,
-      text: "Visit the bustling marketplace, interacting with the locals or possibly purchasing necessary adventuring gear.",
-    },
-  ],
 };
 
 export type Choice = {
-  id: number;
-  text: string;
+    id: number;
+    text: string;
 };
 
 export type MessageData = {
-  content: string[];
-  // TODO: Remove this, it's not needed
-  step: number;
+    content: string[];
+    // TODO: Remove this, it's not needed
+    step: number;
 };
 
 export type UIMessage = {
-  message: MessageData;
-  choices: Choice[];
-  isLoaded?: boolean;
+    message: MessageData;
+    choices: Choice[];
 };
 
 export type MessagesStore = {
-  messages: UIMessage[];
-  addMessage: (message: UIMessage) => void;
-  currentMessage: UIMessage;
-  setCurrentMessage: (message: UIMessage) => void;
+    messages: UIMessage[];
+    addMessage: (message: UIMessage) => void;
+    currentMessage: UIMessage;
+    setCurrentMessage: (message: UIMessage) => void;
 };
 
 export const useMessagesStore = create<MessagesStore>((set) => ({
-  messages: [INITIAL_MESSAGE],
-  addMessage: (message: UIMessage) => {
-    set((state) => ({
-      ...state,
-      messages: [...state.messages, message],
-    }));
-  },
-  currentMessage: INITIAL_MESSAGE,
-  setCurrentMessage: (message: UIMessage) => {
-    set((state) => ({
-      ...state,
-      currentMessage: message,
-    }));
-  },
+    messages: [INITIAL_MESSAGE],
+    addMessage: (message: UIMessage) => {
+        set((state) => ({
+            ...state,
+            messages: [...state.messages, message],
+        }));
+    },
+    currentMessage: INITIAL_MESSAGE,
+    setCurrentMessage: (message: UIMessage) => {
+        set((state) => ({
+            ...state,
+            currentMessage: message,
+        }));
+    },
 }));
